a2e77942c32aa8a21cd83557003b412990d79750
Fernando <fernandobarcalarodriguez@gmail.com>
2020-09-13 12:04:59 +0200

    details

---
+++ SwordFlow/Assets/Scripts/GameController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public PlayerMovement player;
+    public Text gameOverText;
+
+    private bool reloading = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player == null && !reloading)
+        {
+            reloading = true;
+            StartCoroutine(reloadScene());
+        }
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Quit();
+        }
+    }
+
+    public IEnumerator reloadScene()
+    {
+        gameOverText.enabled = true;
+
+        yield return new WaitUntil(() => Input.GetButtonDown("Submit"));
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    void Quit()
+    {
+    #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+    #else
+        Application.Quit();
+    #endif
+    }
+
+}
--- SwordFlow/Assets/Scripts/Player/PlayerMovement.cs
+++ SwordFlow/Assets/Scripts/Player/PlayerMovement.cs
@@ -6,7 +6,6 @@ public class PlayerMovement : MonoBehaviour
     public CharacterController2D controller;
     public float runSpeed = 40f;
     public float rollingDuration = 0.01f;
-    public Text gameOverText;
 
     private float horizontalMove = 0f;
     private bool jump = false;
@@ -61,8 +60,4 @@ public class PlayerMovement : MonoBehaviour
         jump = false;
     }
 
-    private void OnDestroy()
-    {
-        gameOverText.enabled = true;
-    }
 }
